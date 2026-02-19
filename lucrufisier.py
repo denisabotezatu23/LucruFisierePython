0c6883f65b664aac242fac07b6a0ca9ecae5a622
Denisa-Elena Botezatu <denisa-elena.botezatu@student.tuiasi.ro>
2026-02-19 16:07:33 +0000

    Filtrarea cuvintelor de lungime >4

--- lucrufisier.py
+++ lucrufisier.py
@@ -7,9 +7,14 @@ def main():
         if c not in semnepunctuatie:
             continut_final+=c
 
+    cuvinte=continut.split()
+    rezultat=" ".join([c for c in cuvinte if len(c)>4])
+
     f.close()
     f=open("fisiertest.txt","w")
-    f.write(continut_final)
+
+
+    f.write(rezultat)
 
 
 if __name__ == "__main__":
